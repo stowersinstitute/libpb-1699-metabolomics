5c049099f7ee2ea11e36b4916e4db712fe209301
0u812 <0u812@users.noreply.github.com>
2020-09-29 19:04:49 -0500

    clean up

--- src/python/pca-global.py
+++ src/python/pca-global.py
@@ -43,32 +43,20 @@ astyanax_data = astyanax_data.loc[:,['pools' not in c for c in astyanax_data.col
 astyanax_data = astyanax_data.apply(log10)
 astyanax_data.columns = (f'{c} {n}' for c,n in zip(astyanax_data.columns,list(range(1,7))*27))
 astyanax_data = astyanax_data.reindex(sorted(astyanax_data.columns), axis=1)
-#print(astyanax_data)
 
 # get data and compute common metabolites
 astyanax_lipids = ali.lmdata.iloc[:,ali.non_numeric_cols-1:]
 astyanax_lipids = astyanax_lipids.apply(log10)
 astyanax_lipids.columns = (c[0] + ' ' + c[-2] + ' ' + ' '.join(c[1:-2]) + ' ' + c[-1] for c in (cc.split() for cc in astyanax_lipids.columns))
 astyanax_lipids = astyanax_lipids.reindex(sorted(astyanax_lipids.columns), axis=1)
-#astyanax_lipids.columns = astyanax_data.columns
-#print(astyanax_lipids)
 
-#print(astyanax_data.columns,astyanax_lipids.columns)
 astyanax_data = concat((astyanax_data,astyanax_lipids),axis=0)
-#print(astyanax_data)
 
 outliers = ['Tinaja Liver Refed 6', 'Pachon Muscle Refed 5', 'Pachon Liver 30d Starved 3']
 
 if args.exclude_outlier:
     for o in outliers:
         astyanax_data = astyanax_data.loc[:,~astyanax_data.columns.str.contains(o)]
-
-#astyanax_data = DataFrame(StandardScaler().fit_transform(astyanax_data.values), index=astyanax_data.index, columns=astyanax_data.columns)
-
-#gridspec_kw = {"height_ratios":[1.,1.], "width_ratios" : [1.]}
-#fig,ax = plt.subplots(nrows=2,ncols=1,figsize=(8, 12), gridspec_kw=gridspec_kw, projection='3d')
-#fig = plt.figure()
-#fig = plt.figure(figsize=plt.figaspect(0.5))
 
 first_cmap = LinearSegmentedColormap.from_list('First', ["#224b67", "#3498db", "#98cbee"])
 second_cmap = LinearSegmentedColormap.from_list('Second', ["#493054", "#9b59b6", "#e2bdf1"])
@@ -91,7 +79,6 @@ def plot_pca(astyanax_data,selectors,subset_category,ax):
         for color_pos,var2 in selectors:
             p = components.iloc[joined_dataset.columns.isin(astyanax_data.columns) & joined_dataset.columns.str.contains(tissue) & joined_dataset.columns.str.contains(var2)]
             label = ', '.join((tissue, var2))
-            #print(p)
             #https://www.thetopsites.net/article/52439590.shtml
             ax.scatter(p['Comp1'],p['Comp2'],p['Comp3'], label=label, depthshade=False, color=cmap(color_pos))
             handles, labels = ax.get_legend_handles_labels()
