3f1cc768cde355c85f4b6f2e8b4eb6a527493f54
0u812 <0u812@users.noreply.github.com>
2020-10-04 19:22:45 -0500

    still fails

--- src/python/objtables/objtables_test2.py
+++ src/python/objtables/objtables_test2.py
@@ -2,16 +2,37 @@ from obj_tables import (Model, TableFormat, StringAttribute, UrlAttribute, OneTo
 import obj_tables
 from obj_tables.io import Writer
 
+class Address(obj_tables.Model):
+    street = obj_tables.StringAttribute(unique=True, primary=True, verbose_name='Street')
+    city = obj_tables.StringAttribute(verbose_name='City')
+    state = obj_tables.StringAttribute(verbose_name='State')
+    zip_code = obj_tables.StringAttribute(verbose_name='Zip code')
+    country = obj_tables.StringAttribute(verbose_name='Country')
+
+    class Meta(obj_tables.Model.Meta):
+        table_format = obj_tables.TableFormat.multiple_cells
+        attribute_order = ('street', 'city', 'state', 'zip_code', 'country',)
+        verbose_name = 'Address'
+        verbose_name_plural = 'Addresses'
+
 class Company(obj_tables.Model):
     name = obj_tables.StringAttribute(unique=True, primary=True, verbose_name='Name')
+    url = obj_tables.UrlAttribute(verbose_name='URL')
+    address = obj_tables.OneToOneAttribute(Address, related_name='company', verbose_name='Address')
 
-    class Meta(Model.Meta):
+    class Meta(obj_tables.Model.Meta):
         table_format = obj_tables.TableFormat.column
-        attribute_order = ('name',)
+        attribute_order = ('name', 'url', 'address',)
         verbose_name = 'Company'
         verbose_name_plural = 'Companies'
 
-google = Company(name='Google')
+google = Company(name='Google',
+                 url='https://www.google.com/',
+                 address=Address(street='1600 Amphitheatre Pkwy',
+                                 city='Mountain View',
+                                 state='CA',
+                                 zip_code='94043',
+                                 country='US'))
 
 class Observation(Model):
     name = StringAttribute(unique=True,primary=True)
